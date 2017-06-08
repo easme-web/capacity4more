fa0a23fbb27322f91a411c1b09dc4446432431f3
Kevin Van Ransbeeck <climacon@gmail.com>
2017-06-08 15:37:09 +0200

    CAPFDEV-1233: Fix loading homepage AS after initializing.
    Issues left/introduced:
    - clicking anywhere in the AS causes error
    - clicking "load more" at the bottom causes error
    - clickin on a filter (my groups/projects/interests) causes error
    - AS on group dashboard is incorrect (shows articles and group content of other groups) === AS is the same in all contexts (any groups/global)

--- project/profiles/capacity4more/modules/c4m/message/c4m_message/c4m_message_ajax.js
+++ project/profiles/capacity4more/modules/c4m/message/c4m_message/c4m_message_ajax.js
@@ -1,23 +1,47 @@
+/**
+ * @file
+ * Load activity stream via Ajax.
+ */
+
 (function ($) {
+    "use strict";
+
     /**
      * Load remote content after the main page loaded.
      */
     Drupal.behaviors.my_module_load_remote_content = {
-        attach: function(context, settings) {
-            $('#activity-wrapper').once('activity-wrapper', function() {
+        attach: function (context, settings) {
+            $("#activity-wrapper").once("activity-wrapper", function () {
 
-                var base = $(this).attr('id');
+                var base = $(this).attr("id");
 
                 var element_settings = {
-                    url: window.location.protocol + '//' + window.location.hostname +  settings.basePath + settings.pathPrefix + 'ajax/remote',
-                    event: 'click',
+                    url: window.location.protocol + "//" + window.location.hostname + settings.basePath + settings.pathPrefix + "ajax/remote",
+                    event: "click",
                     progress: {
-                        type: 'throbber'
+                        type: "throbber"
                     }
                 };
                 Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
                 $(this).click();
             });
+
+            // When Ajax returns successfully, initiate the activity stream (Angular) controller.
+            $(document).ajaxComplete(function (event, response, settings) {
+
+                var responses = JSON.parse(response.responseText);
+
+                responses.forEach(function (response) {
+                    if ("selector" in response) {
+                        if (response.selector === "#activity-wrapper") {
+                            angular.bootstrap(document.getElementsByTagName("html")[0], ["c4mApp"]);
+                        }
+                    }
+                });
+
+            });
         }
     };
-})(jQuery);
+
+})
+(jQuery);
